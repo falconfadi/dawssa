1645459d44ec86f682ba09dbd7c75ec13268fb69
Reda Shehab <reda.123.rrr@gmail.com>
2024-02-26 15:49:55 +0300

    edit order index

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -26,25 +26,25 @@
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-
                 <table id="example1" class="table table-bordered table-striped">
                     <thead>
                     <tr>
                         <th>#</th>
                         <th>المشترك</th>
+                        <th>المتقدم</th> <!-- Added new column for the applicant -->
                         <th>التاريخ</th>
                         <th>نظامي\مخالفات</th>
                         <th>تحكم</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <?php $i=0; foreach ($orders as $order){
-                    ?>
+                    <?php $i=0; foreach ($orders as $order){ ?>
                     <tr>
                         <td>{{$order->id}}</td>
-                        <td>   <?=$order->client->first_name?>                 </td>
-                        <td><?=$order['created_at']?></td>
-                        <td> <?=($order['is_regular']==0)?'نظامي':'مخالفات'?></td>
+                        <td>{{$order->client->first_name}} {{$order->client->father_name}} {{$order->client->last_name}}</td> <!-- Display complete client name -->
+                        <td>{{$order->applicant_first_name}} {{$order->applicant_father_name}} {{$order->applicant_last_name}}</td> <!-- Display complete applicant name -->
+                        <td>{{$order['created_at']}}</td>
+                        <td>{{($order['is_regular']==0)?'نظامي':'مخالفات'}}</td>
                         <td>
                             <div class="btn-group">
                                 <button type="button" class="btn btn-default"></button>
@@ -59,14 +59,11 @@
                                     <a type="button" class="btn-sm" href="#">تأكيد</a>
                                     <div class="dropdown-divider"></div>
                                     <a type="button" class="btn-sm" href="{{url('admin/orders-profile/'.$order->id)}}">بروفايل</a>
-
                                 </div>
                             </div>
                         </td>
                     </tr>
                     <?php $i++; }?>
-
-
                     </tbody>
                     <tfoot>
                     <tr>
@@ -79,6 +76,7 @@
                     </tfoot>
                 </table>
             </div>
+
             <!-- /.card-body -->
         </div>
         <!-- /.card -->
